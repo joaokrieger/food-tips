3b553d6eca8451b634ea248cdbd9996e67795c43
JEKrieger <joaoeduardokrieger123@gmail.com>
2023-05-26 22:44:08 -0300

    Inicio do desenvolvimento do dashboard

--- lib/home.dart
+++ lib/home.dart
@@ -16,136 +16,325 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF024424),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 20.0),
-            Container(
-              width: 350,
-              height: 400,
-              decoration: BoxDecoration(
-                color: Color(0xFFF0F0F0),
-                borderRadius: BorderRadius.circular(10), // Define o raio das bordas
-              ),
-              child:(
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:[
-                      SizedBox(height: 10.0),
-                      Row(
-                        children: [
-                          SizedBox(width: 16.0),
-                          Text(
-                              'Dados Biométricos',
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 80.0),
+              Container(
+                width: 350,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF0F0F0),
+                  borderRadius: BorderRadius.circular(10), // Define o raio das bordas
+                ),
+                child:(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children:[
+                        SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            SizedBox(width: 16.0),
+                            Text(
+                                'Dados Biométricos',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF504848),
+                                ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                            children:[
+                              SizedBox(
+                                  width: 16.0,
+                                  height: 10.0,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 16.0,
+                                    height: 16.0,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Cristiano Ronaldo',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Color(0xFF504848),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: 16.0,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Homem Adulto',
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontStyle: FontStyle.italic,
+                                          color: Color(0xFF504848),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ]
+                        ),
+                        SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            SizedBox(width: 16.0),
+                            Text(
+                              "Índice de IMC",
                               style: TextStyle(
                                 fontSize: 20,
                                 fontWeight: FontWeight.bold,
                                 color: Color(0xFF504848),
                               ),
-                          ),
-                        ],
-                      ),
-                    ]
-                )
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: SizedBox(
+                                  height: 20,
+                                  child: LinearProgressIndicator(
+                                    value: 0.5, // Valor do progresso (entre 0.0 e 1.0)
+                                    valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF0C9522)), // Cor do progresso
+                                    backgroundColor: Color(0xFFD9D9D9), // Cor de fundo
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            SizedBox(width: 16.0),
+                            Text(
+                              'Status:',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF504848),
+                              ),
+                            ),
+                            SizedBox(width: 10.0),
+                            Text(
+                              'Saudável',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0xFF504848),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: Color(0xFF024424),
+                                    height: 80,
+                                    width: 80,// Defina a altura desejada para a caixa
+                                  ),
+                                  // Outros widgets dentro da coluna
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: Color(0xFF024424),
+                                    height: 80,
+                                    width: 80,// Defina a altura desejada para a caixa
+                                  ),
+                                  // Outros widgets dentro da coluna
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: Color(0xFF024424),
+                                    height: 80,
+                                    width: 80,// Defina a altura desejada para a caixa
+                                  ),
+                                  // Outros widgets dentro da coluna
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: Color(0xFF024424),
+                                    height: 80,
+                                    width: 80,// Defina a altura desejada para a caixa
+                                  ),
+                                  // Outros widgets dentro da coluna
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: Color(0xFF024424),
+                                    height: 80,
+                                    width: 80,// Defina a altura desejada para a caixa
+                                  ),
+                                  // Outros widgets dentro da coluna
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    color: Color(0xFF024424),
+                                    height: 80,
+                                    width: 80,// Defina a altura desejada para a caixa
+                                  ),
+                                  // Outros widgets dentro da coluna
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ]
+                  )
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            SizedBox(
-              height: 60.0,
-              width: 350.0,
-              child: ElevatedButton(
+              SizedBox(height: 30),
+              SizedBox(
+                height: 60.0,
+                width: 350.0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFF0F0F0), // Define a cor de fundo
+                    onPrimary: Color(0xFF504848), // Define a cor do texto
+                  ),
+                  onPressed: () {
+                    // Ação do botão Alimentos
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FoodCategory()),
+                    );
+                  },
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children:[
+                        SizedBox(width: 5),
+                        Image.asset(
+                          'assets/icons/icn_food.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                        SizedBox(width: 50),
+                        Text(
+                          'Alimentos',
+                          style: TextStyle(fontSize: 20)
+                        ),
+                      ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              SizedBox(
+                height: 60.0,
+                width: 350.0,
+                child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   primary: Color(0xFFF0F0F0), // Define a cor de fundo
                   onPrimary: Color(0xFF504848), // Define a cor do texto
                 ),
                 onPressed: () {
-                  // Ação do botão Alimentos
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FoodCategory()),
-                  );
+                  // Ação do botão Recomendações
                 },
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children:[
-                      SizedBox(width: 5),
                       Image.asset(
-                        'assets/icons/icn_food.png',
-                        width: 40,
-                        height: 40,
-                      ),
-                      SizedBox(width: 50),
-                      Text(
-                        'Alimentos',
-                        style: TextStyle(fontSize: 20)
-                      ),
-                    ],
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            SizedBox(
-              height: 60.0,
-              width: 350.0,
-              child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFFF0F0F0), // Define a cor de fundo
-                onPrimary: Color(0xFF504848), // Define a cor do texto
-              ),
-              onPressed: () {
-                // Ação do botão Recomendações
-              },
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children:[
-                    Image.asset(
-                      'assets/icons/icn_recommendation.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                    SizedBox(width: 20),
-                    Text(
-                      'Recomendações',
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ]
-              ),
-            ),
-            ),
-            SizedBox(height: 16.0),
-            SizedBox(
-              height: 60.0,
-              width: 350.0,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFF0F0F0), // Define a cor de fundo
-                  onPrimary: Color(0xFF504848), // Define a cor do texto
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Register()),
-                  );
-                },
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children:[
-                      SizedBox(width: 10),
-                      Image.asset(
-                        'assets/icons/icn_user.png',
-                        width: 35,
-                        height: 35,
+                        'assets/icons/icn_recommendation.png',
+                        width: 50,
+                        height: 50,
                       ),
                       SizedBox(width: 20),
                       Text(
-                        'Dados de Usuário',
+                        'Recomendações',
                         style: TextStyle(fontSize: 20),
                       )
                     ]
                 ),
               ),
-            ),
-          ],
+              ),
+              SizedBox(height: 16.0),
+              SizedBox(
+                height: 60.0,
+                width: 350.0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFF0F0F0), // Define a cor de fundo
+                    onPrimary: Color(0xFF504848), // Define a cor do texto
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Register()),
+                    );
+                  },
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children:[
+                        SizedBox(width: 10),
+                        Image.asset(
+                          'assets/icons/icn_user.png',
+                          width: 35,
+                          height: 35,
+                        ),
+                        SizedBox(width: 20),
+                        Text(
+                          'Dados de Usuário',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ]
+                  ),
+                ),
+              ),
+              SizedBox(height: 40.0)
+            ],
+          ),
         ),
       ),
     );
--- lib/index.dart
+++ lib/index.dart
@@ -11,87 +11,92 @@ void main() {
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF024424),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/white_logo.png',
-              height: 300.0,
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'CPF',
-                labelStyle: TextStyle(
-                  color: Colors.black45, // Defina a cor desejada aqui
+    return
+      Scaffold(
+        backgroundColor: Color(0xFF024424),
+
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 100.0),
+                Image.asset(
+                  'assets/white_logo.png',
+                  height: 300.0,
                 ),
-                filled: true,
-                fillColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Senha',
-                labelStyle: TextStyle(
-                  color: Colors.black45, // Defina a cor desejada aqui
-                ),
-                filled: true,
-                fillColor: Colors.white,
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0,),
-            SizedBox(
-              height: 50.0,
-              width: 200.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Implementar a lógica de login aqui
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Home()),
-                  );
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 20,
+                SizedBox(height: 10.0),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'CPF',
+                    labelStyle: TextStyle(
+                      color: Colors.black45, // Defina a cor desejada aqui
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
                   ),
                 ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF008445)), // Altere para a cor desejada
+                SizedBox(height: 16.0),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(
+                      color: Colors.black45, // Defina a cor desejada aqui
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  obscureText: true,
                 ),
-              ),
+                SizedBox(height: 16.0,),
+                SizedBox(
+                  height: 50.0,
+                  width: 200.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Implementar a lógica de login aqui
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF008445)), // Altere para a cor desejada
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Não tem uma conta?"),
-              SizedBox(width: 4.0),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Register()),
-                  );
-                },
-                child: Text('Cadastre-se'),
-              ),
-            ],
           ),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 50.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Não tem uma conta?"),
+                SizedBox(width: 4.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Register()),
+                    );
+                  },
+                  child: Text('Cadastre-se'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
